c787498178da319134f7f0f0c300a0d80c75cfab
Kurt Böhm <kurbo96@gmail.com>
2025-12-13 18:49:03 +0100

    Update colours

--- demo/snippets/demo.cpp
+++ demo/snippets/demo.cpp
@@ -2,10 +2,8 @@
 #include <cstdint>
 #include <cstdlib>
 #include <new>
+#include <print>
 #include <string>
-
-#include <fmt/core.h>
-#include <fmt/format.h>
 
 template<std::integral T>
 struct Abc {
@@ -22,6 +20,6 @@ struct Abc {
 int main(int argc, const char** argv) {
   uint8_t* data = new (std::align_val_t(64)) uint8_t[64];
   Abc abc{.a = 0, .b = 32, .c = "abc"};
-  fmt::print("{}: {}\n", argv[0], argc);
+  std::print("{}: {}\n", argv[0], argc);
   return EXIT_SUCCESS;
 }
